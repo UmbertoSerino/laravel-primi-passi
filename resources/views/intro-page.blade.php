077afa9e56d67845f4c66c7b88142ae02e735272
UmbertoSerino <serumberto@gmail.com>
2024-01-31 15:32:10 +0100

    first commit

---
+++ resources/views/intro-page.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro Page</title>
+</head>
+
+<body>
+    <h1>
+        Intro Page by Laravel
+    </h1>
+    <a href="http://127.0.0.1:8000">Torna alla pagina precedente</a>
+</body>
+
+</html>
